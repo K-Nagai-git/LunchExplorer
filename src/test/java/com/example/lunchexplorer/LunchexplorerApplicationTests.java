79b26c9a49d9673f549d68080c09fc8f5e20226a
3030823 <3030823@30308-23>
2024-10-10 13:06:32 +0900

    グループ演習

---
+++ src/test/java/com/example/lunchexplorer/LunchexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lunchexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
