2cae35123e60f3168c75cff989aaf64c2769a4a5
3030927 <3030927@30309-27>
2024-10-15 11:05:04 +0900

    2回目

--- src/main/java/com/example/lunchex/entity/Users.java
+++ src/main/java/com/example/lunchex/entity/Users.java
@@ -1,14 +1,13 @@
 // 作成　深田　1015　メモ：エンティティの作成　お土産をもらった
 
 package com.example.lunchex.entity;
-import lombok.Data;
 
 @Date
 public class Users {
 	
-	    private int userId;        // ユーザーID（主キー）
-	    private String nickname;   // ニックネーム（必須）
-	    private String password;   // パスワード（必須）
+	    private String user_mail;        //ユーザーメールアドレス（PK）
+	    private String user_nickname ;   // ニックネーム（必須）
+	    private String user_password ;   // パスワード（必須）
 	
 	
 	
