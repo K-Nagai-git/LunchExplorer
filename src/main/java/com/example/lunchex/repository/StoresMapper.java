38e4167db25a983050bbe16dc931c0a4cff4116a
3030838 <3030838@30308-38>
2024-10-22 09:37:28 +0900

    StoresMapper作成

--- src/main/java/com/example/lunchex/repository/StoresMapper.java
+++ src/main/java/com/example/lunchex/repository/StoresMapper.java
@@ -2,13 +2,17 @@ package com.example.lunchex.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.lunchex.entity.Stores;
+
 @Mapper
 public interface StoresMapper {
 	/**店舗IDで１件取得*/
-	Stores 
+	Stores getStoreById(Stores stores);
 	/**登録*/
+	Stores addStore(Stores stores);
 	/**更新*/
+	Stores updateStore(Stores stores);
 	/**削除*/
-
+	Stores deleteStore(Stores stores);
 	
 }
