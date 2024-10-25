33bbe57188991909c2755088a48d8d8f8d3d4a72
3030928 <3030928@30309-28>
2024-10-25 13:45:52 +0900

    飲食店登録画面の画面プレビューのHのページ

--- src/main/java/com/example/lunchex/service/StoresService.java
+++ src/main/java/com/example/lunchex/service/StoresService.java
@@ -1,11 +1,9 @@
-//作成 木和田 1016
-//更新 深田   1017 メモ：クラス名が「StoreService」→「StoresService」に更新
-
 package com.example.lunchex.service;
 
 import java.util.List;
 
 import com.example.lunchex.entity.Stores;
+import com.example.lunchex.form.StoresForm.StoreRegisterForm;
 
 public interface StoresService {
     
@@ -13,14 +11,14 @@ public interface StoresService {
     List<Stores> getAllStores();
     
     // IDで指定された店舗情報を取得する
-    Stores getStoreById(int store̠id);
+    Stores getStoreById(int storeId);  
     
     // 新しい店舗を追加する
-    void addStore(Stores stores);
-    
+    void addStore(StoreRegisterForm storeForm);  
+
     // 既存の店舗情報を更新する
     void updateStore(Stores stores);
     
-    // 店舗を削除する
-    void deleteStore(int store̠id);
+    // 既存の店舗を削除する
+    void deleteStore(int storeId);  
 }
