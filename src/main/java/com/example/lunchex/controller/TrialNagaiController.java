0d5978639429541d296a08346cb14e80c959a451
3030823 <3030823@30308-23>
2024-10-29 08:49:29 +0900

    detail修正

--- src/main/java/com/example/lunchex/controller/TrialNagaiController.java
+++ src/main/java/com/example/lunchex/controller/TrialNagaiController.java
@@ -1,10 +1,14 @@
 package com.example.lunchex.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.lunchex.entity.Detail;
 import com.example.lunchex.repository.DetailMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -109,58 +113,85 @@ public class TrialNagaiController {
 	//** DI *	
 	private final DetailMapper detailMapper;
 
-	//	//** 指定されたIDの詳細を表示
-	@GetMapping
-	public String detail(Model model) {
-		model.addAttribute("lunchex", detailMapper.getDetailsByUser(2));
-		System.out.println();
-		System.out.println(model);
-		System.out.println();		
-		return "trial_nagai";
-	}
-}
+	//	//** レビュー全件を表示
+	//	@GetMapping
+	//	public String allDetail(Model model) {
+	//		model.addAttribute("lunchex", detailMapper.selectAllDetail());
+	//		System.out.println();
+	//		System.out.println(model);
+	//		System.out.println();		
+	//		return "trial_nagai";
+	//	}
+	//}
 
-//	** 新規登録
-//@GetMapping
-//public String newStore(Model model) {
-//	Stores stores = new Stores();
-//	stores.setStore_name("俺のフレンチ");
-//	stores.setStore_tel("9876-5732");
-//	stores.setStore_address("京都市～");
-//	stores.setStore_url("www.abc.com");
-//	stores.setUser_mail("kiwada@gmail.com");           
-//	storesMapper.addStore(stores);
-//	System.out.println("=== 登録確認 ===");
-//	model.addAttribute("lunchex", storesMapper.getStoreById(9));
-//	System.out.println(storesMapper.getStoreById(9));
-//	System.out.println();
-//	//return "trial_nagai";
-//	//	}
-//	//}
-//	// ★更新
-//	Stores target = storesMapper.getStoreById(9);
-//	target.setStore_name("あたいのイタリアン");
-//	target.setStore_tel("0000-0000");
-//	target.setStore_address("心斎橋～");
-//	target.setStore_url("www.def.com");
-//	target.setUser_mail("kiwada@gmail.com");          
-//	storesMapper.updateStore(target);
-//	System.out.println("=== 更新確認 ===");
-//	System.out.println(storesMapper.getStoreById(9));
-//	System.out.println();
-//	//		return "trial_nagai";
-//	//		}
-//	//	}
-//
-//	//	 ★削除
-//	storesMapper.deleteStore(9);
-//	System.out.println("=== 削除確認 ===");
-//	System.out.println(storesMapper.getStoreById(8));
-//	model.addAttribute("lunchex", storesMapper.getStoreById(8));
-//	System.out.println(storesMapper.getStoreById(9));
-////	model.addAttribute("lunchex", storesMapper.getStoreById(9));
-//	System.out.println();
-//	return "trial_nagai";
-//}	
-//}
+	//	//** 指定されたIDのレビューを表示
+	//	@GetMapping
+	//	public String selectDetail(Model model) {
+	//		model.addAttribute("lunchex", detailMapper.selectByIdDetail(2));
+	//		System.out.println();
+	//		System.out.println(model);
+	//		System.out.println();		
+	//		return "trial_nagai";
+	//	}
+	//}
+
+	//	** 新規登録
+	@GetMapping
+	public String newDetail(Model model) {
+		Detail detail = new Detail();
+
+		detail.setStore_id(2);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate date = LocalDate.parse("2024-10-28", formatter);			
+		detail.setDetail_postdt(date);
+		detail.setUser_mail("kiwada@gmail.com");
+		detail.setDetail_menu("あんぱん");
+		detail.setDetail_price(100);
+		detail.setDetail_rating(3);		
+		detail.setDetail_review("テスト、テスト");
+		detail.setDetail_usedt(date);
+		detail.setDetail_visits(3);
+
+		detailMapper.insertDetail(detail);
+		System.out.println("=== 登録確認 ===");
+		model.addAttribute("lunchex", detailMapper.selectByIdDetail(12));
+		System.out.println(detailMapper.selectByIdDetail(12));
+		System.out.println();
+		//		return "trial_nagai";
+		//	}
+		//}
+
+		//	// ★更新
+		Detail target = detailMapper.selectByIdDetail(12);
+		target.setStore_id(2);
+		//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		//		LocalDate date = LocalDate.parse("2024-10-28", formatter);			
+		target.setDetail_postdt(date);
+		target.setUser_mail("kiwada@gmail.com");
+		target.setDetail_menu("クリームぱん");
+		target.setDetail_price(150);
+		target.setDetail_rating(3);		
+		target.setDetail_review("お試し");
+		target.setDetail_usedt(date);
+		target.setDetail_visits(3);
+
+		detailMapper.updateDetail(target);
+		System.out.println("=== 更新確認 ===");
+		System.out.println(detailMapper.selectByIdDetail(12));
+		System.out.println();
+		//		return "trial_nagai";
+		//	}
+		//}
+		//
+		//	//	 ★削除
+		detailMapper.deleteDetail(12);
+		System.out.println("=== 削除確認 ===");
+		System.out.println(detailMapper.selectAllDetail());
+		model.addAttribute("lunchex", detailMapper.selectAllDetail());
+		//		System.out.println(detailMapper.selectByIdDetail(12));
+		//	model.addAttribute("lunchex", storesMapper.getStoreById(9));
+		System.out.println();
+		return "trial_nagai";
+	}	
+}
 //
--- src/main/java/com/example/lunchex/repository/DetailMapper.java
+++ src/main/java/com/example/lunchex/repository/DetailMapper.java
@@ -1,21 +1,60 @@
 package com.example.lunchex.repository;
 /**作成日：2024/10/22  */
 /**作成者：糸山  		*/
-/**更新日：  			*/
-/**更新者：  			*/
+/**更新： 2024/10/28　永井　  			*/
+
+
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.lunchex.entity.Detail;
 
+/**
+ * Detail：リポジトリ
+ */
 @Mapper
 public interface DetailMapper {
-	/***/
-	/**店舗IDで１件取得*/
-	Detail getDetailsByUser(int id);
-	/**登録*/
-	void addDetail(Detail detail);
-	/**更新*/
-	void updateDetail(Detail detail);
-	/**削除*/
-	void deleteDetail(int id);
+
+    /**
+     * 全ての「レビュー等」を取得します。
+     */
+    List<Detail> selectAllDetail();
+
+    /**
+     * 指定されたIDに対応する「レビュー等」を取得します。
+     */
+    Detail selectByIdDetail(@Param("id") Integer id);
+
+    /**
+     * 「レビュー等」を登録します。
+     */
+    void insertDetail(Detail detail);
+
+    /**
+     * 「レビュー等」を更新します。
+     */
+    void updateDetail(Detail detail);
+
+    /**
+     * 指定されたIDの「レビュー等」を削除します。
+     */
+    void deleteDetail(@Param("id") Integer id);
 }
+
+
+//*****************
+//
+//@Mapper
+//public interface DetailMapper {
+//	/***/
+//	/**店舗IDで１件取得*/
+//	Detail getDetailsByUser(int id);
+//	/**登録*/
+//	void addDetail(Detail detail);
+//	/**更新*/
+//	void updateDetail(Detail detail);
+//	/**削除*/
+//	void deleteDetail(int id);
+//}
